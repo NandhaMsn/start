588fc615bcc7d440c97ad80b7b1b43f02741c7fe
NandhaMsn <93091250+NandhaMsn@users.noreply.github.com>
2023-12-13 22:53:40 +0530

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '@styles/globals.css'
+import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Layout from './../components/Layout';
+import dynamic from 'next/dynamic'
 
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
+  ssr: false
+});
+
+
+function MyApp({ Component, pageProps }) {
+  return <Layout > <Component {...pageProps} /><AnimatedCursor
+  innerSize={8}
+  outerSize={55}
+  innerScale={1}
+  outerScale={2}
+  outerAlpha={0}
+  hasBlendMode={true}
+  innerStyle={{
+    backgroundColor: "#560ff3"
+  }}
+  outerStyle={{
+    border: '3px solid #560ff317',
+    backgroundColor: '#560ff333'
+  }}
+/></Layout>
 }
 
-export default Application
+export default MyApp
